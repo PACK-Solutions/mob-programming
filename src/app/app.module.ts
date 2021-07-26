afcd39b0a4bd40956f30d507d6f7da4cdf509f46
magnasilvar <3359919+magnasilvar@users.noreply.github.com>
2021-07-26 16:14:46 +0200

    fix: update i18n folder prefix path
    And move upload dist folder to latest build step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,40 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/comp/header.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AddCollaboratorComponent } from './settings/collaborator/comp/add-collaborator/add-collaborator.component';
 import { HomeBasicComponent } from './home/comp/home-basic/home-basic.component';
+import { MainListComponent } from './main/main-list/main-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AddCollaboratorComponent } from './settings/collaborator/comp/add-collaborator/add-collaborator.component';
+import { ArrayCollaboratorComponent } from './settings/collaborator/comp/array-collaborator/array-collaborator.component';
+import { EditCollaboratorComponent } from './settings/collaborator/comp/edit-collaborator/edit-collaborator.component';
+import { RemoveCollaboratorComponent } from './settings/collaborator/comp/remove-collaborator/remove-collaborator.component';
+import { RotationCollaboratorComponent } from './settings/collaborator/comp/rotation-collaborator/rotation-collaborator.component';
+import { ShuffleCollaboratorComponent } from './settings/collaborator/comp/shuffle-collaborator/shuffle-collaborator.component';
 import { SettingsComponent } from './settings/settings.component';
-
-import { MatButtonModule } from '@angular/material/button';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatMenuModule } from '@angular/material/menu';
-
-import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { AddTimerComponent } from './settings/timer/comp/add-timer/add-timer.component';
 import { EditTimerComponent } from './settings/timer/comp/edit-timer/edit-timer.component';
 import { PauseStartTimerComponent } from './settings/timer/comp/pause-start-timer/pause-start-timer.component';
 import { ResetTimerComponent } from './settings/timer/comp/reset-timer/reset-timer.component';
-
-import { ArrayCollaboratorComponent } from './settings/collaborator/comp/array-collaborator/array-collaborator.component';
-import { ShuffleCollaboratorComponent } from './settings/collaborator/comp/shuffle-collaborator/shuffle-collaborator.component';
-import { RemoveCollaboratorComponent } from './settings/collaborator/comp/remove-collaborator/remove-collaborator.component';
-import { EditCollaboratorComponent } from './settings/collaborator/comp/edit-collaborator/edit-collaborator.component';
-import { RotationCollaboratorComponent } from './settings/collaborator/comp/rotation-collaborator/rotation-collaborator.component';
-
-import { MainListComponent } from './main/main-list/main-list.component';
-
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TimerPipe } from './settings/timer/pipe/timer.pipe';
-
 
 @NgModule({
   declarations: [
@@ -104,5 +95,5 @@ export class AppModule { }
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, './assets/i18n/');
 }
