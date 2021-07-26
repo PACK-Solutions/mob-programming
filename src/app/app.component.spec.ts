58463c9d379c4d5ebcc81ecbeb8e525a5e6d30ca
magnasilvar <3359919+magnasilvar@users.noreply.github.com>
2021-07-26 23:26:30 +0200

    feat: use browser language or English by default
    English and French are supported

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,33 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { AppComponent } from './app.component';
-import { DebugElement, NO_ERRORS_SCHEMA } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
-import { MatMenuModule } from '@angular/material/menu';
-import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
-  let app: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
-  let de: DebugElement;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AppComponent ],
-      imports: [RouterTestingModule, TranslateModule.forRoot(), MatMenuModule]
-    })
-      .compileComponents();
-  });
+  let translateService: TranslateService;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        TranslateModule.forRoot()
+      ]
+    }).compileComponents();
+    translateService = TestBed.inject(TranslateService);
     fixture = TestBed.createComponent(AppComponent);
-    app = fixture.debugElement.componentInstance;
-    de = fixture.debugElement;
-    fixture.detectChanges();
   });
 
+  it('should use "fr" lang', () => {
+    spyOn(translateService, 'getBrowserLang').and.returnValue('fr');
+    spyOn(translateService, 'use').and.stub();
+    fixture.componentInstance.ngOnInit();
+    expect(translateService.use).toHaveBeenCalledWith('fr');
   });
+
+  it('should use default lang', () => {
+    spyOn(translateService, 'getBrowserLang').and.returnValue('unknown');
+    spyOn(translateService, 'use').and.stub();
+    fixture.componentInstance.ngOnInit();
+    expect(translateService.use).not.toHaveBeenCalled();
+  });
+
+});
