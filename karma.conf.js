e59fe7207f1db6a1ccd16bc214fb7932d53756bb
magnasilvar <3359919+magnasilvar@users.noreply.github.com>
2021-07-26 09:56:25 +0200

    ci: add node.js ci action
    Containing build, test and sonarcloud scan steps

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage/MobProgramming'),
       subdir: '.',
       reporters: [
-        { type: 'html' },
+        { type: 'lcov' },
         { type: 'text-summary' }
       ]
     },
